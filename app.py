6c0b4530b1c6d2f9d9e7809ab2edf3fda3ec9d92
vbijan <bijan.gopali@gmail.com>
2026-02-24 13:15:38 -0600

    updated app

--- app.py
+++ app.py
@@ -27,22 +27,25 @@ def dictionary():
     3. If not found, find all approximate matches and return
     """
 
-    word = request.args.get("word")
+    words = request.args.getlist("word")
 
-    if not word:
+    if not words:
         return jsonify({"data": "Not a valid word or no word provided."})
     
-    # exact match
-    definations = match_exact(word)
-    if definations:
-        return jsonify({"status": "success", "data": definations, "word": word})
-    
-    definations = match_like(word)
-    if definations:
-        return jsonify({"status": "partial", "data":definations, "word": word})
-    else:
-        return jsonify({"status": "error", "data": "word not found", "word": word})
+    response = {"words": []}
 
+    for word in words:
+        # exact match
+        definations = match_exact(word)
+        if definations:
+            response["words"].append({"status": "success", "word":word, "Data":definations})
+        else:
+            definations = match_like(word)
+            if definations:
+                response["words"].append({"status": "partial", "data":definations, "word": word})
+            else:
+                response["words"].append({"status": "error", "data": "word not found", "word": word})
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
